0dcad3874a4aef1d7a31bf2b24ff2def11a666a5
KhalilSacranie <khalilsacranie991@gmail.com>
2021-10-07 16:35:09 +0100

    initial setup

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const { Client, Intents, Collection } = require('discord.js');
+require('dotenv').config({ path: 'src/.env' });
+const token = process.env.BOT_TOKEN;
+const prefix = process.env.PREFIX;
+const fs = require('fs');
+
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('src/commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('messageCreate', (message) => {
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        client.commands.get('ping').execute(message, args);
+
+    } else if (command === 'roll') {
+        client.commands.get('roll').execute(message, args);
+
+    } else if (command === 'purge') {
+        client.commands.get('purge').execute(message, args);
+
+    } else if (command === 'userinfo') {
+        client.commands.get('userinfo').execute(message, args, client)
+    }
+
+});
+
+client.login(token);
