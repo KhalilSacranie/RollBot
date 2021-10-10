dd6ee7aa167a016f491e0d3b2aaed4f7e98c6162
KhalilSacranie <khalilsacranie991@gmail.com>
2021-10-10 17:46:14 +0100

    streamlined stats.js still working on updating the json

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,36 +1,7 @@
-// module.exports = {
-//     name: 'stats',
-//     execute(message, args, client) {
-//         const jsonObj = {Id: '1', Link: "https://cdn.discordapp.com/attachments/895699041030119565/896105984102121523/Black.png"};
-//         const currentstatsheet = jsonObj.Link;
-
-//         if (args[0] === 'update') {
-
-//             if (!message.author.id === ['460200007522320394', '543084123460337675']) {
-//                 message.reply('You do not have permission to do that.');
-
-//             } else {
-//                 if (!args[1]){
-//                     message.reply('Please enter a link to the new statsheet');
-//                 } else if (args[1]) {
-//                     const newstatsheet = args[1];
-
-//                     message.reply(newstatsheet)
-
-
-//             }
-
-//         } else if (!args[0]) {
-//             message.channel.send(currentstatsheet);
-//         }
-//     }
-// }
-// }
-
 module.exports = {
     name: 'stats',
     execute(message, args, client) {
-        const sheet = {statsheet: "https://cdn.discordapp.com/attachments/895699041030119565/896105984102121523/Black.png"};
+        const sheet = {statsheet: "https://cdn.discordapp.com/attachments/881247911340568627/896481905334190080/unknown.png"};
         const currentstatsheet = sheet.statsheet;
 
         if (args [0] === 'update') {
