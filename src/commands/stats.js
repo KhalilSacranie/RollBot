b403aa7127a07ff39a35f1eb8b43acc0e25c7444
KhalilSacranie <khalilsacranie991@gmail.com>
2021-10-11 18:43:36 +0100

    start of stats overhall

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -2,30 +2,68 @@ module.exports = {
     name: 'stats',
     execute(message, args, client) {
         const sheet = {statsheet: "https://cdn.discordapp.com/attachments/881247911340568627/896481905334190080/unknown.png"};
-        const currentstatsheet = sheet.statsheet;
+        const ppl = ['460200007522320394']
 
         if (args [0] === 'update') {
             
-            if (!message.author.id === ['460200007522320394', '543084123460337675']) {
-                message.reply('You do not have permission to do that.');
-                return;
+            // if (!message.author.id === ['460200007522320394', '543084123460337675']) {
+            //     message.reply('You do not have permission to do that.');
+            //     return;
 
-            } else if (message.author.id === ['460200007522320394', '543084123460337675']) {
+            // } else if (message.author.id === ['460200007522320394', '543084123460337675']) {
 
-                if (!args[1]) {
-                    message.reply('Please enter the new statsheet link.');
-                    return;
+            //     if (!args[1]) {
+            //         message.reply('Please enter the new statsheet link.');
+            //         return;
                 
-                } else if (args[1]) {
-                    newstatsheet = args[1];
-                    console.log(newstatsheet);
-                    sheet.statsheet = newstatsheet;
-                }
+            //     } else if (args[1]) {
+            //         newstatsheet = args[1];
+            //         console.log(newstatsheet);
+            //         sheet.statsheet = newstatsheet;
+            //     }
+            // }
+
+            if(message.author.id === ppl) {
+                message.channel.send('yes')
+
+            } else {
+                message.channel.send('no')
             }
 
         } else if (!args[0]) {
-            message.channel.send(currentstatsheet);
+            message.channel.send(sheet.statsheet);
 
         }
+    }
+}
+
+const fs = require('fs');
+
+module.exports = {
+    name: 'stats',
+    execute(message, args, client) {
+        const accessid = [];
+
+        if (args[1] === 'update') {
+            return;
+
+        } else {
+            fs.readFile('../data/data.json', 'utf-8', (err, jsonString) => {
+                try {
+                    try {
+                        const data = JSON.parse(jsonString);
+                        console.log(data.content);
+
+                    } catch (err) {
+                        console.log(err);
+                    }
+
+                } catch (err) {
+                    console.log(err);
+
+                }
+            });
+        }
+
     }
 }
