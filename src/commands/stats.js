74477c5aef4c37efc95521217844d12a2514f642
KhalilSacranie <khalilsacranie991@gmail.com>
2021-10-11 19:29:05 +0100

    ITS DONE FINNALY

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,51 +1,36 @@
-module.exports = {
-    name: 'stats',
-    execute(message, args, client) {
-        const sheet = {statsheet: "https://cdn.discordapp.com/attachments/881247911340568627/896481905334190080/unknown.png"};
-        const ppl = ['460200007522320394']
-
-        if (args [0] === 'update') {
-            
-            // if (!message.author.id === ['460200007522320394', '543084123460337675']) {
-            //     message.reply('You do not have permission to do that.');
-            //     return;
-
-            // } else if (message.author.id === ['460200007522320394', '543084123460337675']) {
-
-            //     if (!args[1]) {
-            //         message.reply('Please enter the new statsheet link.');
-            //         return;
-                
-            //     } else if (args[1]) {
-            //         newstatsheet = args[1];
-            //         console.log(newstatsheet);
-            //         sheet.statsheet = newstatsheet;
-            //     }
-            // }
-
-            if(message.author.id === ppl) {
-                message.channel.send('yes')
-
-            } else {
-                message.channel.send('no')
-            }
-
-        } else if (!args[0]) {
-            message.channel.send(sheet.statsheet);
-
-        }
-    }
-}
-
 const fs = require('fs');
 
 module.exports = {
     name: 'stats',
     execute(message, args, client) {
-        const accessid = [];
 
-        if (args[1] === 'update') {
-            return;
+        const accessid = ['543084123460337675', '460200007522320394'];
+
+        if (args[0] === 'update') {
+            if (message.author.id === '543084123460337675') {
+                if(!args[1]) {
+                    message.reply('Please enter the link for the new stat sheet.');
+
+                } else {
+                    const link = args[1];
+                    const newStatSheet = {
+                        name: 'statSheet',
+                        content: link,
+                    };
+
+                    fs.writeFile('src/data/statSheet.json', JSON.stringify(newStatSheet), err => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log('New stat sheet successfully saved!')
+                        }
+                    });
+                }
+
+            } else {
+                message.reply('You do not have permission to do that!');
+                return;
+            }
 
         } else {
             fs.readFile('src/data/statSheet.json', 'utf-8', (err, jsonString) => {
@@ -54,11 +39,10 @@ module.exports = {
 
                 } else {
                     try {
-                        const data = JSON.parse(jsonString);
-                        message.channel.send(data)
-
+                        const statSheet = JSON.parse(jsonString);
+                        message.channel.send(statSheet);
                     } catch (err) {
-                        console.log(err)
+                        console.log(err);
                     }
                 }
             });
