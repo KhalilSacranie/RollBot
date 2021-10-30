272ac74dac110087fa0811fcb9ab23b40041d1a1
KhalilSacranie <khalilsacranie991@gmail.com>
2021-10-30 21:20:26 +0100

    Update terminate.js

--- src/commands/terminate.js
+++ src/commands/terminate.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'terminate',
     execute(message, args) {
         if (message.author.id === '543084123460337675') {
-            message.channel.send('Byeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
+            await message.channel.send('Byeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
             process.exit(1);
 
         } else {
