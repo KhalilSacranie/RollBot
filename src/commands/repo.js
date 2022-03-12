db0320659e6fc4a683a8f8eecdef1c69323f0b90
KhalilSacranie <khalilsacranie991@gmail.com>
2022-03-12 22:13:34 +0000

    a

--- src/commands/repo.js
+++ src/commands/repo.js
@@ -10,10 +10,31 @@ module.exports = {
                 .setTitle('RollBot\'s Github Repo')
                 .setColor(message.guild.me.displayHexColor)
                 .setDescription("https://github.com/KhalilSacranie/RollBot")
-                .addField("License", `MIT License\n\nCopyright (c) 2022 Khalil Sacranie\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the "Software"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.`)
+                .addField("License", "\u200b")
+                .addField("MIT License", "\u200b")
+                .addField("\u200b", "\u200b")
+                .addField("Copyright (c) 2022 Khalil Sacranie", "\u200b")
+                .addField("\u200b", "\u200b")
+                .addField("Permission is hereby granted, free of charge, to any person obtaining a copy", "\u200b")
+                .addField("of this software and associated documentation files (the \"Software\"), to deal", "\u200b")
+                .addField("in the Software without restriction, including without limitation the rights", "\u200b")
+                .addField("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell", "\u200b")
+                .addField("copies of the Software, and to permit persons to whom the Software is", "\u200b")
+                .addField("furnished to do so, subject to the following conditions:", "\u200b")
+                .addField("\u200b", "\u200b")
+                .addField("The above copyright notice and this permission notice shall be included in all", "\u200b")
+                .addField("copies or substantial portions of the Software.", "\u200b")
+                .addField("\u200b", "\u200b")
+                .addField("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR", "\u200b")
+                .addField("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,", "\u200b")
+                .addField("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE", "\u200b")
+                .addField("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER", "\u200b")
+                .addField("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,", "\u200b")
+                .addField("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE", "\u200b")
+                .addField("SOFTWARE.", "\u200b")
                 .setThumbnail(client.user.displayAvatarURL())
                 .setTimestamp()
-            
+
             message.channel.send({ embeds: [Embed] });
 
         } catch (err) {
